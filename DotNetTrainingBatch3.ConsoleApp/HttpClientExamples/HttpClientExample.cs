f9c281b76c3348eff4efd33901abc353cd237c34
Lwin Nandar Oo <lwinnandaroo21@gmail.com>
2024-02-25 21:11:08 +0630

    HttpClient CRUD

--- DotNetTrainingBatch3.ConsoleApp/HttpClientExamples/HttpClientExample.cs
+++ DotNetTrainingBatch3.ConsoleApp/HttpClientExamples/HttpClientExample.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 using DotNetTrainingBatch3.ConsoleApp.Models;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
@@ -14,7 +16,12 @@ namespace DotNetTrainingBatch3.ConsoleApp.HttpClientExamples
         public async Task Run()
         {
             //await Read();
-            await ReadJsonPlaceHolder();
+            //await ReadJsonPlaceHolder();
+            //await Edit(1);
+            //await Create("title1", "author1", "content1");
+            //await Update(18,"title2", "author2", "content2");
+            await Delete(18);
+
         }
 
         private async Task Read()
@@ -40,6 +47,10 @@ namespace DotNetTrainingBatch3.ConsoleApp.HttpClientExamples
                     Console.WriteLine(item.BlogContent);
 
                 }
+            } 
+            else
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
             }
         }
 
@@ -67,6 +78,110 @@ namespace DotNetTrainingBatch3.ConsoleApp.HttpClientExamples
                     Console.WriteLine(item.userId);
 
                 }
+            }
+        }
+
+        private async Task Edit(int id)
+        {
+            string url = $"https://localhost:7163/api/Blog/{id}";
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                
+
+                BlogModel item = JsonConvert.DeserializeObject<BlogModel>(jsonStr)!;
+
+                
+                    Console.WriteLine(item.BlogId);
+                    Console.WriteLine(item.BlogTitle);
+                    Console.WriteLine(item.BlogAuthor);
+                    Console.WriteLine(item.BlogContent);
+
+                
+            }
+            else
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        private async Task Create (string title, string author, string content)
+        {
+            BlogModel blog = new BlogModel()
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };
+
+            string jsonBlog = JsonConvert.SerializeObject(blog);
+
+            HttpContent httpContent = new StringContent(jsonBlog, Encoding.UTF8, MediaTypeNames.Application.Json);
+            
+            string url = $"https://localhost:7163/api/Blog";
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.PostAsync(url,httpContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+
+            }
+            else
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        private async Task Update(int id, string title, string author, string content)
+        {
+            BlogModel blog = new BlogModel()
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };
+
+            string jsonBlog = JsonConvert.SerializeObject(blog);
+
+            HttpContent httpContent = new StringContent(jsonBlog, Encoding.UTF8, MediaTypeNames.Application.Json);
+
+            string url = $"https://localhost:7163/api/Blog/{id}";
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.PutAsync(url, httpContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+
+            }
+            else
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        private async Task Delete(int id)
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage httpResponseMessage = await httpClient.DeleteAsync($"https://localhost:7163/api/Blog/{id}");
+            HttpResponseMessage response = httpResponseMessage;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
             }
         }
     }
