fd8015ceacd267d52b4d931a2da995ed0a8f9faf
Lwin Nandar Oo <lwinnandaroo21@gmail.com>
2024-03-10 22:13:39 +0630

    [Complete] Asp.Net Core Mvc Blog Read

---
+++ DotNetTrainingBatch3.MvcApp/AddDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetTrainingBatch3.MvcApp.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetTrainingBatch3.MvcApp;
+
+public class AddDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "TestDb",
+            UserID = "sa",
+            Password = "sa@123",
+            TrustServerCertificate = true
+        };
+        optionsBuilder.UseSqlServer(sqlConnectionStringBuilder.ConnectionString);
+    }
+
+    public DbSet<BlogModel> Blogs { get; set; }
+}
---
+++ DotNetTrainingBatch3.MvcApp/Controllers/BlogController.cs
@@ -0,0 +1,24 @@
+﻿using DotNetTrainingBatch3.MvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTrainingBatch3.MvcApp.Controllers
+{
+    public class BlogController : Controller
+    {
+
+        private readonly AddDbContext _context;
+
+        public BlogController ()
+        {
+            _context = new AddDbContext ();
+        }
+
+        //https://localhost:7187/Blog/Index
+        [ActionName("Index")]
+        public IActionResult BlogIndex()
+        {
+            List<BlogModel> lst = _context.Blogs.ToList();
+            return View("BlogIndex",lst);
+        }
+    }
+}
